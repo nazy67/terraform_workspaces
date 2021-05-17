243e062bbbf8e56f4a838d7cac47ca2eca68f0de
nazy67 <nazykh67@gmail.com>
2021-05-17 02:05:59 -0500

    modified main.tf, backend.tf, outputs.tf

--- rds_&_webserver/web_workspace/backend.tf
+++ rds_&_webserver/web_workspace/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-nazy-state"
-    key    = "webserver.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "terraform-state-locks"
+    bucket               = "terraform-nazy-state"
+    key                  = "webserver.tfstate"
+    region               = "us-east-1"
+    dynamodb_table       = "terraform-state-locks"
     workspace_key_prefix = "ws-homework"
   }
 }
