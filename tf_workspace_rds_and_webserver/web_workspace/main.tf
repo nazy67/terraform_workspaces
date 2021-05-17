35cf0b75a59f0e54b17a084fa2b139df87f66445
nazy67 <nazykh67@gmail.com>
2021-05-17 01:33:36 -0500

    modified main.tf

--- tf_workspace_rds_and_webserver/web_workspace/main.tf
+++ tf_workspace_rds_and_webserver/web_workspace/main.tf
@@ -1,5 +1,5 @@
 module "webserver_module" {
-  source = "github.com/nazy67/terraform/aws_terraform_modules//modules/web_server"
+  source = "github.com/nazy67/terraform//modules/webserver"
   env = terraform.workspace
   instance_type = "t2.micro"
   
@@ -9,41 +9,4 @@ module "webserver_module" {
       region = "us-east-1"
       workspace_key_prefix = "ws-homework"
   } 
-}
-
-
-
-output "alb_dns" { 
-  value = module.webserver_module.module_alb_dns
-}
-output "webserver_launch_template_name" {
-  value = module.webserver_module.module_launch_template_name
-  description = "the name of webserver launch template name"
-} 
-output "webserver_launch_template_id" {
-  value = module.webserver_module.module_launch_template_id
-  description = "the name of webserver launch template id"
-} 
-output "webserver_asg_name" {
-  value = module.webserver_module.module_web_asg_name
-  description = "the name of webserver asg"
-} 
-output "webserver_asg_arn" {
-  value = module.webserver_module.module_web_asg_arn
-  description = "webserver's asg id"
-}
-output "webserver_alb_arn" {
-  value = module.webserver_module.module_web_alb_arn
-  description = "webserver's load balancer arn"
-}
-output "webserver_alb_sg_name" {
-  value = module.webserver_module.module_web_alb_sg_name
-  description = "webserver's load balancer security group name"
-}
-output "target_group_name" {
-   value =  module.webserver_module.module_tg_name
-   description = "this is a webserver's target group name"
-}
-output "http_listener_arn"{
-  value =  module.webserver_module.module_http_listeners_arn 
 }
