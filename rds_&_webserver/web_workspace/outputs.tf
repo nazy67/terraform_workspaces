af06a865715e20fb1bc1347715b788bccb096c0e
nazy67 <nazykh67@gmail.com>
2021-05-17 23:44:15 -0500

    modified outputs.tf

--- rds_&_webserver/web_workspace/outputs.tf
+++ rds_&_webserver/web_workspace/outputs.tf
@@ -1,42 +1,18 @@
 output "alb_dns" {
-  value = module.webserver_module.module_alb_dns
+  value = module.webserver_module.web_alb_dns
 }
 
-output "webserver_launch_template_name" {
-  value       = module.webserver_module.module_launch_template_name
-  description = "the name of webserver launch template name"
-}
-
-output "webserver_launch_template_id" {
-  value       = module.webserver_module.module_launch_template_id
+output "web_lt_id" {
+  value       = module.webserver_module.web_lt_id
   description = "the name of webserver launch template id"
 }
 
-output "webserver_asg_name" {
-  value       = module.webserver_module.module_web_asg_name
-  description = "the name of webserver asg"
-}
-
-output "webserver_asg_arn" {
-  value       = module.webserver_module.module_web_asg_arn
+output "web_asg_arn" {
+  value       = module.webserver_module.web_asg_arn
   description = "webserver's asg id"
 }
 
-output "webserver_alb_arn" {
-  value       = module.webserver_module.module_web_alb_arn
+output "web_alb_arn" {
+  value       = module.webserver_module.web_alb_arn
   description = "webserver's load balancer arn"
-}
-
-output "webserver_alb_sg_name" {
-  value       = module.webserver_module.module_web_alb_sg_name
-  description = "webserver's load balancer security group name"
-}
-
-output "target_group_name" {
-  value       = module.webserver_module.module_tg_name
-  description = "this is a webserver's target group name"
-}
-
-output "http_listener_arn" {
-  value = module.webserver_module.module_http_listeners_arn
 }
